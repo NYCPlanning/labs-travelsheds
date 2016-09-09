ee6cee17cb112471a77a8ea70c29f45d667ef223
Chris Whong <chris@whong.org>
2016-09-09 14:33:35 -0400

    added shp download

--- js/script.js
+++ js/script.js
@@ -1,3 +1,84 @@
+var isochroneFC
+
+zip.workerScriptsPath = 'js/zipWorkerScripts/'
+
+
+$('.download-button').click(function() {
+
+  var options = {
+    folder: 'myshapes',
+    types: {
+        point: 'mypoints',
+        polygon: 'mypolygons',
+        line: 'mylines'
+    }
+  }
+
+
+  var msDataset = mapshaper.internal.importGeoJSON(isochroneFC, {
+    auto_snap:false,
+    files:['input.json'],
+    no_repair:false
+  })
+
+  var files = mapshaper.internal.exportShapefile(msDataset, {
+    format: "shapefile"
+  })
+
+  
+
+  saveZipFile('travelsheds.zip',files, function(){
+  })
+
+  //from mapshaper-gui.js
+ function saveZipFile(zipfileName, files, done) {
+    var toAdd = files;
+    try {
+      zip.createWriter(new zip.BlobWriter("application/zip"), addFile, zipError);
+    } catch(e) {
+      // TODO: show proper error message, not alert
+      done("This browser doesn't support Zip file creation.");
+    }
+
+    function zipError(msg) {
+      var str = "Error creating Zip file";
+      if (msg) {
+        str += ": " + (msg.message || msg);
+      }
+      done(str);
+    }
+
+    function addFile(archive) {
+      if (toAdd.length === 0) {
+        archive.close(function(blob) {
+          saveBlob(zipfileName, blob, done);
+        });
+      } else {
+        var obj = toAdd.pop(),
+            blob = new Blob([obj.content], { type: "text/plain" });
+        archive.add('travelsheds' + obj.filename, new zip.BlobReader(blob), function() {addFile(archive);});
+      }
+    }
+  }
+
+  function saveBlob(filename, blob, done) {
+    //IE11 & Edge
+    if (navigator.msSaveBlob) {
+        navigator.msSaveBlob(csvData, exportFilename);
+    } else {
+        //In FF link must be added to DOM to be clicked
+        var link = document.createElement('a');
+        link.href = window.URL.createObjectURL(blob);
+        link.setAttribute('download', filename);
+        document.body.appendChild(link);    
+        link.click();
+        //document.body.removeChild(link);    
+    }
+    done();
+  }
+
+})
+  
     mapboxgl.accessToken = 'pk.eyJ1IjoiY3dob25nbnljIiwiYSI6ImNpczF1MXdrdjA4MXcycXA4ZGtyN2x5YXIifQ.3HGyME8tBs6BnljzUVIt4Q';
     var map = new mapboxgl.Map({
         container: 'map',
@@ -8,11 +89,28 @@
         pitch: 0
     })
 
-    map.on('style.load', function() {
+    //manually instantiate cartodb map
 
+
+    map.on('style.load', function() {    
+      $.ajax({
+        type: 'POST',
+        url: 'http://cwhong.carto.com/api/v1/map/named/tpl_230e29ac_7640_11e6_89c5_0e05a8b3e3d7',
+        dataType : "text",
+        contentType: "application/json",
+        success: function(data) { 
+          data = JSON.parse(data);
+          var layergroupid = data.layergroupid
+
+          setupMapLayers(layergroupid)
+        }
+      })
+    })
+
+    function setupMapLayers(layergroupid) {
       map.addSource('subway-lines', {
         type: 'raster',
-        tiles: ['https://cartocdn-ashbu.global.ssl.fastly.net/cwhong/api/v1/map/cwhong@4dff0d5e@e3f7aba54afc7cda617961b8152d7560:1468296486061/1/{z}/{x}/{y}.png'],
+        tiles: ['https://cwhong.carto.com/api/v1/map/' + layergroupid + '/{z}/{x}/{y}.png'],
         tileSize: 256
       })
 
@@ -62,7 +160,6 @@
 
 
       map.on('click', function(e){
-        console.log(e)
         map.getSource('dropped-pin').setData({
           type: 'Feature',
           geometry: {
@@ -72,13 +169,15 @@
         })
 
         getIsochrone(e.lngLat);
-      })  
-    })
+      }) 
+    }
+
+    
+  
 
 
 
 function getIsochrone(lngLat) {
-  console.log(lngLat)
   var apiCall = Mustache.render('http://{{host}}/otp/routers/default/isochrone?routeId=default&batch=true&fromPlace={{lat}},{{lng}}&date=2016/09/25&time=12:00:00&mode=TRANSIT,WALK&cutoffSec=900&cutoffSec=1800&cutoffSec=2700', {
       lat: lngLat.lat,
       lng: lngLat.lng,
@@ -86,7 +185,8 @@ function getIsochrone(lngLat) {
   })
 
   $.getJSON(apiCall, function(geojson) {
-    console.log(geojson)
+
+    isochroneFC = geojson
 
     map.getSource('transitsheds').setData(geojson)
 
