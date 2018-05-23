968a9229c7bae731023f6ce3bd4de984fbdc8f4b
Chris Whong <chris@whong.org>
2018-05-22 23:22:31 -0400

    update for use with new otp server, add eslint

--- js/script.js
+++ js/script.js
@@ -1,316 +1,297 @@
-var isochroneFC
-var lngLat
-zip.workerScriptsPath = 'js/zipWorkerScripts/'
-
-    mapboxgl.accessToken = 'pk.eyJ1IjoiY3dob25nbnljIiwiYSI6ImNpczF1MXdrdjA4MXcycXA4ZGtyN2x5YXIifQ.3HGyME8tBs6BnljzUVIt4Q';
-    var map = new mapboxgl.Map({
-        container: 'map',
-        style: 'mapbox://styles/mapbox/light-v9',
-        zoom: 10,
-        minZoom: 1,
-        center: [-74.024849,40.705628],
-        pitch: 0
-    })
-    map.addControl(new mapboxgl.Navigation());
-
-    //manually instantiate cartodb map
-
-
-    map.on('style.load', function() {    
-      $.ajax({
-        type: 'POST',
-        url: 'https://cwhong.carto.com/api/v1/map/named/tpl_230e29ac_7640_11e6_89c5_0e05a8b3e3d7',
-        dataType : "text",
-        contentType: "application/json",
-        success: function(data) { 
-          data = JSON.parse(data);
-          var layergroupid = data.layergroupid
-
-          setupMapLayers(layergroupid)
-        }
-      })
-    })
-
-    function setupMapLayers(layergroupid) {
-      map.addSource('subway-lines', {
-        type: 'raster',
-        tiles: ['https://cwhong.carto.com/api/v1/map/' + layergroupid + '/{z}/{x}/{y}.png'],
-        tileSize: 256
-      })
-
-      map.addLayer({
-            "id": "subway-lines",
-            "type": "raster",
-            "source": "subway-lines",
-            "minzoom": 0,
-            "maxzoom": 22
-      })
-
-      map.addSource('dropped-pin', {
-        type: 'geojson',
-        data: {
-          type: 'Feature',
-          geometry: null
-        }
-      })
-
-
-
-      map.addSource('transitsheds', {
-        type: 'geojson',
-        data: {
-          type: 'Feature',
-          geometry: null
-        }
-      })
-
-      map.addLayer({
-        "id": "transitsheds",
-        "type": "fill",
-        "source": "transitsheds",
-        "paint": {
-          'fill-color': 'steelblue',
-          'fill-opacity': 0.3,
-          'fill-outline-color': 'gray'     
-        }
-      });    
-
-      map.addLayer({
-        "id": "dropped-pin2",
-        "type": "circle",
-        "source": "dropped-pin",
-        "paint": {
-            "circle-radius": 6,
-            "circle-color": "gray"
-        }
-      }); 
-
-      map.addLayer({
-        "id": "dropped-pin",
-        "type": "circle",
-        "source": "dropped-pin",
-        "paint": {
-            "circle-radius": 5,
-            "circle-color": "orange"
-        }
-      }); 
-
-      
-
-
-      map.on('click', function(e){
-
-
-        lngLat = e.lngLat
-        getIsochrone();
-      }) 
-    }
-
-// Event Handlers 
-  
-window.mode = 'TRANSIT,WALK'
-
-$( ".mode-select" ).change(function(e) {
-  mode=e.target.value
-})
-
-$('.clear-button').click(function() {
-  map.getSource('transitsheds').setData({
-    type: 'FeatureCollection',
-    features: []
-  })
-
-  map.getSource('dropped-pin').setData({
-    type: 'Feature',
-    geometry: null
-  })
-
-  //disable download buttons
-  $('.download-button').attr('disabled', true)
-  $('.redraw-button').attr('disabled', true)
-
-  //restore default message
-  $('.message').show()
-  $('.clear-button').hide()
-
-})
-
-$('.redraw-button').click(function() {
-  getIsochrone()
-})
-
-$('.download-shp').click(function() {
-
-  var options = {
-    folder: 'myshapes',
-    types: {
-        point: 'mypoints',
-        polygon: 'mypolygons',
-        line: 'mylines'
-    }
-  }
-
-  //create a mapshaper dataset from geojson FeatureCollection
-  var msDataset = mapshaper.internal.importGeoJSON(isochroneFC, {
-    auto_snap:false,
-    files:['input.json'],
-    no_repair:false
-  })
-
-  //create an array of ArrayBuffers (one per file in the shapefile)
-  var files = mapshaper.internal.exportShapefile(msDataset, {
-    format: "shapefile"
-  })
-
-  // //manually create a .prj file for WGS84
-  // not working properly, excluding prj for now...
-  // var crsString = 'GEOGCS["GCS_WGS_1984",DATUM["D_WGS_1984",SPHEROID["WGS_1984",6378137,298.257223563]],PRIMEM["Greenwich",0],UNIT["Degree",0.017453292519943295]]'
- 
-  // var buf = new ArrayBuffer(crsString.length*2)
-  // var bufView = new Uint16Array(buf);
-
-  // for (var i=0, strLen=crsString.length; i<strLen; i++) {
-  //   bufView[i] = crsString.charCodeAt(i);
-  // }
-
-  // files.push({
-  //   content: buf,
-  //   filename: '.prj'
-  // })
-  
-
-  saveZipFile('travelsheds.zip',files, function(){
-  })
-
-  //combine all files into a zip archive
-  //from mapshaper-gui.js
- function saveZipFile(zipfileName, files, done) {
-    var toAdd = files;
-    try {
-      zip.createWriter(new zip.BlobWriter("application/zip"), addFile, zipError);
-    } catch(e) {
-      // TODO: show proper error message, not alert
-      done("This browser doesn't support Zip file creation.");
-    }
-
-    function zipError(msg) {
-      var str = "Error creating Zip file";
-      if (msg) {
-        str += ": " + (msg.message || msg);
-      }
-      done(str);
-    }
-
-    function addFile(archive) {
-      if (toAdd.length === 0) {
-        archive.close(function(blob) {
-          saveBlob(zipfileName, blob, done);
-        });
-      } else {
-        var obj = toAdd.pop(),
-            blob = new Blob([obj.content], { type: "text/plain" });
-        archive.add('travelsheds' + obj.filename, new zip.BlobReader(blob), function() {addFile(archive);});
-      }
-    }
-  }
-
- 
-
-
-})
-
-
-
-$('.download-geojson').click(function() {
-    //prepare blob from geojson
-    var blob = new Blob([JSON.stringify(isochroneFC)], { type: "text/plain" });
-    saveBlob('travelsheds.geojson', blob, function() {})
-})
-
+let isochroneFC;
+zip.workerScriptsPath = 'js/zipWorkerScripts/';
 
 function saveBlob(filename, blob, done) {
-  //IE11 & Edge
+  // IE11 & Edge
   if (navigator.msSaveBlob) {
-      navigator.msSaveBlob(blob, filename);
+    navigator.msSaveBlob(blob, filename);
   } else {
-      //In FF link must be added to DOM to be clicked
-      var link = document.createElement('a');
-      link.href = window.URL.createObjectURL(blob);
-      link.setAttribute('download', filename);
-      document.body.appendChild(link);    
-      link.click();
-      //document.body.removeChild(link);    
+    // In FF link must be added to DOM to be clicked
+    const link = document.createElement('a');
+    link.href = window.URL.createObjectURL(blob);
+    link.setAttribute('download', filename);
+    document.body.appendChild(link);
+    link.click();
+    // document.body.removeChild(link);
   }
   done();
 }
 
-function getIsochrone() {
-
-
-  $('#loading-overlay').fadeIn()
-  map.setLayoutProperty('transitsheds', 'visibility', 'none')
+function getIsochrone(map, lngLat) {
+  $('#loading-overlay').fadeIn();
+  map.setLayoutProperty('transitsheds', 'visibility', 'none');
 
   map.getSource('dropped-pin').setData({
     type: 'Feature',
     geometry: {
       type: 'Point',
-      coordinates: [lngLat.lng, lngLat.lat]
+      coordinates: [lngLat.lng, lngLat.lat],
+    },
+  });
+
+  $('.download-button').attr('disabled', true);
+
+
+  // prepare a date
+  const day = $('.day-select').val();
+  const hour = $('.hour-select').val();
+
+  let date = '2018/05/21';
+
+  if (day === 'saturday') date = '2018/05/19';
+  if (day === 'sunday') date = '2018/05/20';
+
+  const time = `${hour}:00:00`;
+
+
+  let apiCall = Mustache.render('https://{{host}}/otp/routers/default/isochrone?routeId=default&batch=true&fromPlace={{lat}},{{lng}}&date={{{date}}}&time={{time}}&mode={{mode}}', {
+    host: 'otp.planninglabs.nyc',
+    lat: lngLat.lat,
+    lng: lngLat.lng,
+    date,
+    time,
+    mode,
+  });
+
+  cutoffs.forEach((cutoff) => {
+    // check if valid integer
+    if (Number.isInteger(cutoff)) {
+      const seconds = cutoff * 60;
+      apiCall += `&cutoffSec=${seconds}`;
     }
-  })
+  });
 
-  $('.download-button').attr('disabled', true)
+  console.log('Fetching isochrones...', apiCall);
 
+  $.getJSON(apiCall, (geojson) => {
+    isochroneFC = geojson;
 
-  //prepare a date
-  var day = $('.day-select').val()
-  var hour = $('.hour-select').val()
+    map.getSource('transitsheds').setData(geojson);
+    map.setLayoutProperty('transitsheds', 'visibility', 'visible');
+    $('#loading-overlay').fadeOut();
+    $('.download-button').attr('disabled', false);
+    $('.redraw-button').attr('disabled', false);
 
-  var date = day=='saturday' ? '2016/09/24' :
-    day=='sunday' ? '2016/09/25':
-    '2016/09/23'
-
-  var time = hour + ':00:00'
-
-  //use these days for now...
-  //2016/09/23 - weekday
-  //2016/09/24 - saturday
-  //2016/09/25 - sunday
-
-
-
-  var apiCall = Mustache.render('https://{{host}}/otp/routers/default/isochrone?routeId=default&batch=true&fromPlace={{lat}},{{lng}}&date={{{date}}}&time={{time}}&mode={{mode}}', {
-      host: 'otp.capitalplanning.nyc',
-      lat: lngLat.lat,
-      lng: lngLat.lng,
-      date: date,
-      time: time,
-      mode: mode
-  })
-
-  cutoffs.forEach(function(cutoff) {
-    //check if valid integer
-    if(Number.isInteger(cutoff)) {
-      var seconds = cutoff * 60
-      apiCall += '&cutoffSec=' + seconds      
-    }
-  })
-
-  console.log('Fetching isochrones...', apiCall)
-
-  $.getJSON(apiCall, function(geojson) {
-
-    isochroneFC = geojson
-
-    map.getSource('transitsheds').setData(geojson)
-    map.setLayoutProperty('transitsheds', 'visibility', 'visible')
-    $('#loading-overlay').fadeOut()
-    $('.download-button').attr('disabled', false)
-    $('.redraw-button').attr('disabled', false)
-
-    $('.message').hide()
-    $('.clear-button').show()
-
-  })
+    $('.message').hide();
+    $('.clear-button').show();
+  });
 }
 
+function setupMapLayers(map, layergroupid) {
+  map.addSource('transportation', {
+    type: 'vector',
+    tiles: [`https://planninglabs.carto.com/api/v1/map/${layergroupid}/{z}/{x}/{y}.mvt`],
+  });
 
+  const subwayLayers = subwayLayerGroup.layers.map(layer => layer.layer);
+
+  subwayLayers.forEach((layer) => {
+    map.addLayer(layer);
+  });
+
+  map.addSource('dropped-pin', {
+    type: 'geojson',
+    data: {
+      type: 'Feature',
+      geometry: null,
+    },
+  });
+
+  map.addSource('transitsheds', {
+    type: 'geojson',
+    data: {
+      type: 'Feature',
+      geometry: null,
+    },
+  });
+
+  map.addLayer({
+    id: 'transitsheds',
+    type: 'fill',
+    source: 'transitsheds',
+    paint: {
+      'fill-color': 'steelblue',
+      'fill-opacity': 0.3,
+      'fill-outline-color': 'gray',
+    },
+  });
+
+  map.addLayer({
+    id: 'dropped-pin2',
+    type: 'circle',
+    source: 'dropped-pin',
+    paint: {
+      'circle-radius': 6,
+      'circle-color': 'gray',
+    },
+  });
+
+  map.addLayer({
+    id: 'dropped-pin',
+    type: 'circle',
+    source: 'dropped-pin',
+    paint: {
+      'circle-radius': 5,
+      'circle-color': 'orange',
+    },
+  });
+
+
+  map.on('click', (e) => {
+    const { lngLat } = e;
+    getIsochrone(map, lngLat);
+  });
+}
+
+const map = new mapboxgl.Map({
+  container: 'map',
+  style: '//raw.githubusercontent.com/NYCPlanning/labs-gl-style/master/data/style.json',
+  zoom: 10,
+  minZoom: 1,
+  center: [-74.024849, 40.705628],
+  pitch: 0,
+});
+map.addControl(new mapboxgl.NavigationControl());
+
+// manually instantiate cartodb vector tile
+map.on('style.load', () => {
+  $.ajax({
+    type: 'POST',
+    url: 'https://planninglabs.carto.com/api/v1/map',
+    dataType: 'text',
+    data: JSON.stringify({
+      version: '1.3.0',
+      layers: [
+        {
+          id: 'subway-routes',
+          type: 'mapnik',
+          options: {
+            cartocss_version: '2.3.0',
+            cartocss: '#layer { polygon-fill: #FFF; }',
+            sql: 'SELECT the_geom_webmercator, rt_symbol FROM mta_subway_routes_v0',
+          },
+        },
+        {
+          id: 'subway-stops',
+          type: 'mapnik',
+          options: {
+            cartocss_version: '2.3.0',
+            cartocss: '#layer { polygon-fill: #FFF; }',
+            sql: 'SELECT the_geom_webmercator, name FROM mta_subway_stops_v0',
+          },
+        },
+        {
+          id: 'subway-entrances',
+          type: 'mapnik',
+          options: {
+            cartocss_version: '2.3.0',
+            cartocss: '#layer { polygon-fill: #FFF; }',
+            sql: 'SELECT the_geom_webmercator FROM mta_subway_entrances_v0',
+          },
+        },
+      ],
+    }),
+    contentType: 'application/json',
+    success(res) {
+      const { layergroupid } = JSON.parse(res);
+      setupMapLayers(map, layergroupid);
+    },
+  });
+});
+
+// Event Handlers
+
+window.mode = 'TRANSIT,WALK';
+
+$('.mode-select').change((e) => {
+  mode = e.target.value;
+});
+
+$('.clear-button').click(() => {
+  map.getSource('transitsheds').setData({
+    type: 'FeatureCollection',
+    features: [],
+  });
+
+  map.getSource('dropped-pin').setData({
+    type: 'Feature',
+    geometry: null,
+  });
+
+  // disable download buttons
+  $('.download-button').attr('disabled', true);
+  $('.redraw-button').attr('disabled', true);
+
+  // restore default message
+  $('.message').show();
+  $('.clear-button').hide();
+});
+
+$('.redraw-button').click(() => {
+  getIsochrone();
+});
+
+$('.download-shp').click(() => {
+  const options = {
+    folder: 'myshapes',
+    types: {
+      point: 'mypoints',
+      polygon: 'mypolygons',
+      line: 'mylines',
+    },
+  };
+
+  // create a mapshaper dataset from geojson FeatureCollection
+  const msDataset = mapshaper.internal.importGeoJSON(isochroneFC, {
+    auto_snap: false,
+    files: ['input.json'],
+    no_repair: false,
+  });
+
+  // create an array of ArrayBuffers (one per file in the shapefile)
+  const files = mapshaper.internal.exportShapefile(msDataset, {
+    format: 'shapefile',
+  });
+
+  // combine all files into a zip archive
+  // from mapshaper-gui.js
+  function saveZipFile(zipfileName, filesToZip, done) {
+    const toAdd = filesToZip;
+
+    function addFile(archive) {
+      if (toAdd.length === 0) {
+        archive.close((blob) => {
+          saveBlob(zipfileName, blob, done);
+        });
+      } else {
+        const obj = toAdd.pop();
+        const blob = new Blob([obj.content], { type: 'text/plain' });
+        archive.add(`travelsheds${obj.filename}`, new zip.BlobReader(blob), () => { addFile(archive); });
+      }
+    }
+
+    function zipError(msg) {
+      let str = 'Error creating Zip file';
+      if (msg) {
+        str += `: ${msg.message || msg}`;
+      }
+      done(str);
+    }
+
+    try {
+      zip.createWriter(new zip.BlobWriter('application/zip'), addFile, zipError);
+    } catch (e) {
+      // TODO: show proper error message, not alert
+      done("This browser doesn't support Zip file creation.");
+    }
+  }
+
+  saveZipFile('travelsheds.zip', files, () => {});
+});
+
+
+$('.download-geojson').click(() => {
+  // prepare blob from geojson
+  const blob = new Blob([JSON.stringify(isochroneFC)], { type: 'text/plain' });
+  saveBlob('travelsheds.geojson', blob, () => {});
+});
